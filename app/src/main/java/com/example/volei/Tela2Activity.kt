4568723ad85922128d59a67ff79dd7e5f831b945
Thales Ayala <rtlz.thales@gmail.com>
2022-05-26 23:50:42 -0300

    back function

--- app/src/main/java/com/example/volei/Tela2Activity.kt
+++ app/src/main/java/com/example/volei/Tela2Activity.kt
@@ -2,6 +2,7 @@ package com.example.volei
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import org.w3c.dom.Text
 
@@ -9,11 +10,16 @@ class Tela2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
+        val backbutton : Button = findViewById(R.id.backtostart)
         val t2timeA : TextView = findViewById(R.id.t2TimeA)
         val t2timeB: TextView = findViewById(R.id.t2timeB)
         val vPassadoTimA = intent.getStringExtra("nA")
         val vPassadoTimB = intent.getStringExtra("nB")
         t2timeA.text= vPassadoTimA.toString()
         t2timeB.text=vPassadoTimB.toString()
+        backbutton.setOnClickListener(){
+            startActivity(intent)
+        }
+
     }
 }
