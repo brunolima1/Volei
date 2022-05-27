3cd83ebcf4e2e246bbbcccc73ce9a0cf1ffcb488
Thales Ayala <rtlz.thales@gmail.com>
2022-05-26 23:44:09 -0300

    Intents Tela

--- app/src/main/java/com/example/volei/MainActivity.kt
+++ app/src/main/java/com/example/volei/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.volei
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonstart: Button = findViewById(R.id.startpartida)
+        val timeA : EditText = findViewById(R.id.timeA)
+        val timeB : EditText = findViewById(R.id.timeB)
+
+        buttonstart.setOnClickListener(){
+            val intent = Intent(applicationContext,Tela2Activity::class.java)
+            val nomeTimeA = timeA.text.toString()
+            val nomeTimeB = timeB.text.toString()
+            val params1= Bundle()
+            val params2= Bundle()
+            params1.putString("nA",nomeTimeA)
+            params2.putString("nB",nomeTimeB)
+            intent.putExtras(params1)
+            intent.putExtras(params2)
+            startActivity(intent)
+
+
+        }
+
     }
 }
--- app/src/main/java/com/example/volei/Tela2Activity.kt
+++ app/src/main/java/com/example/volei/Tela2Activity.kt
@@ -2,10 +2,18 @@ package com.example.volei
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class Tela2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
+        val t2timeA : TextView = findViewById(R.id.t2TimeA)
+        val t2timeB: TextView = findViewById(R.id.t2timeB)
+        val vPassadoTimA = intent.getStringArrayExtra("nA")
+        val vPassadoTimB = intent.getStringArrayExtra("nB")
+        t2timeA.text= vPassadoTimA.toString()
+        t2timeB.text=vPassadoTimB.toString()
     }
 }
