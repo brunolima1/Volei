476e8aa1f36e9b83250cc8f982b1f1c4ae389f13
brunolima1 <49993384+Negatives-One@users.noreply.github.com>
2022-05-30 09:53:13 -0300

    Recycler view Prepare

--- app/src/main/java/com/example/volei/MainActivity.kt
+++ app/src/main/java/com/example/volei/MainActivity.kt
@@ -17,12 +17,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonstart: Button = findViewById(R.id.startpartida)
+        val buttonStart: Button = findViewById(R.id.startpartida)
+        val buttonResult: Button = findViewById(R.id.startpartida)
         val timeA : EditText = findViewById(R.id.timeA)
         val timeB : EditText = findViewById(R.id.timeB)
 
 
-        buttonstart.setOnClickListener(){
+        buttonStart.setOnClickListener(){
+                val gotoScreen2 = Intent(applicationContext, Tela2Activity::class.java)
+                val nomeTimeA = timeA.text.toString()
+                val nomeTimeB = timeB.text.toString()
+                val params1 = Bundle()
+                val params2 = Bundle()
+                params1.putString("nA", nomeTimeA)
+                params2.putString("nB", nomeTimeB)
+                gotoScreen2.putExtras(params1)
+                gotoScreen2.putExtras(params2)
+                startActivity(gotoScreen2)
+        }
+        buttonResult.setOnClickListener(){
                 val gotoScreen2 = Intent(applicationContext, Tela2Activity::class.java)
                 val nomeTimeA = timeA.text.toString()
                 val nomeTimeB = timeB.text.toString()
